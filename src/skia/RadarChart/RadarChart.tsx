3c8d2878b49da7e95094eb7f57258eda93078d8c
Bashem <mohammed.bashem@gmail.com>
2025-10-12 13:21:29 +0600

    fixed radar chart

--- src/skia/RadarChart/RadarChart.tsx
+++ src/skia/RadarChart/RadarChart.tsx
@@ -16,6 +16,7 @@ import {
 } from '@shopify/react-native-skia';
 import { getPaddings, type CommonStyle } from '../common';
 import useRadarChart from './useRadarChart';
+import { arrayFrom } from '../../util/util';
 
 type Point = { x: number; y: number };
 
@@ -37,6 +38,7 @@ interface RadarChartStyle extends CommonStyle {
 interface RadarChartProps {
   data: RadarDatum[];
   labels?: string[];
+  levels?: number;
   labelViews?: ReactNode[];
   maxValue?: number;
   minValue?: number;
@@ -49,6 +51,7 @@ function RadarChart({
   labelViews,
   maxValue,
   minValue = 0,
+  levels = 5,
   style,
 }: RadarChartProps) {
   const {} = useRadarChart();
@@ -69,21 +72,21 @@ function RadarChart({
   const cx = size / 2;
   const cy = size / 2;
 
-  const angles = useMemo(
-    () =>
-      data[0]?.values.map(
-        (_, index) => -Math.PI / 2 + (index * 2 * Math.PI) / data.length
-      ) ?? [],
-    [data[0]?.values.length]
-  );
-
+  const angles = useMemo(() => {
+    let length = data[0]?.values.length ?? 0;
+    if (length === 0 && labels) length = labels.length;
+    if (length === 0 && labelViews) length = labelViews.length;
+    if (length === 0) return [];
+    return arrayFrom(length, 1).map(
+      (i) => -Math.PI / 2 + (i * 2 * Math.PI) / length
+    );
+  }, [data[0]?.values.length, labels?.length, labelViews?.length]);
+  console.log(angles);
   // helper to calculate (x,y) for given angle and r (from center)
   const pointFor = (angle: number, r: number): Point => ({
     x: cx + Math.cos(angle) * r,
     y: cy + Math.sin(angle) * r,
   });
-
-  const levels = 5;
 
   // Precompute grid paths (concentric polygons)
   const gridPaths = useMemo(() => {
@@ -134,7 +137,7 @@ function RadarChart({
 
   // Label positions (rendered as RN Text overlay)
   const formatedlabels = useMemo(() => {
-		if (labels === undefined) return undefined;
+    if (labels === undefined) return undefined;
     const out: {
       x: number;
       y: number;
@@ -143,14 +146,14 @@ function RadarChart({
     }[] = [];
     const labelRadius = radius + 12;
     angles.forEach((angle, i) => {
-      const pt = pointFor(angle, labelRadius);
-      const deg = (angle * 180) / Math.PI;
+      const point = pointFor(angle, labelRadius);
+      const degree = (angle * 180) / Math.PI;
       let align: 'left' | 'center' | 'right' = 'center';
-      if (deg > -90 && deg < 90)
-        align = 'left';
-      else if (deg > 90 || deg < -90) align = 'right', pt.x = size - pt.x;
+      if (degree > -90 && degree < 90) align = 'left';
+      else if (degree > 90 || degree < -90)
+        ((align = 'right'), (point.x = size - point.x));
       else align = 'center';
-      out.push({ x: pt.x, y: pt.y, label: labels[i]!, align });
+      out.push({ x: point.x, y: size - point.y, label: labels[i]!, align });
     });
     return out;
   }, [angles, radius, labels]);
@@ -165,21 +168,26 @@ function RadarChart({
     }[] = [];
     const labelRadius = radius + 12;
     angles.forEach((angle, i) => {
-      const pt = pointFor(angle, labelRadius);
+      const point = pointFor(angle, labelRadius);
       const degree = (angle * 180) / Math.PI;
       let alignment: 'left' | 'center' | 'right' = 'center';
-      if (degree > -90 && degree < 90)
-        alignment = 'left';
-      else if (degree > 90 || degree < -90) alignment = 'right', pt.x = size - pt.x;
+      if (degree > -90 && degree < 90) alignment = 'left';
+      else if (degree > 90 || degree < -90)
+        ((alignment = 'right'), (point.x = size - point.x));
       else alignment = 'center';
-      out.push({ x: pt.x, y: pt.y, view: labelViews[i]!, align: alignment });
+      out.push({
+        x: point.x,
+        y: size - point.y,
+        view: labelViews[i]!,
+        align: alignment,
+      });
     });
     return out;
   }, [angles, radius, labelViews]);
 
   const strokeWidth = style?.strokeWidth ?? 2;
   const strokeColor = style?.strokeColor ?? 'gray';
-  const centerDotRadius = style?.centerDotRadius ?? 5;
+  const centerDotRadius = style?.centerDotRadius ?? 2;
   const centerDotColor = style?.centerDotColor ?? strokeColor;
 
   return (
@@ -195,9 +203,9 @@ function RadarChart({
       <Canvas style={{ width: size, height: size }}>
         <Group>
           {/* grid polygons */}
-          {gridPaths.map((gridPath, idx) => (
+          {gridPaths.map((gridPath, index) => (
             <Path
-              key={'g' + idx}
+              key={index}
               path={gridPath}
               style="stroke"
               strokeWidth={strokeWidth}
@@ -205,9 +213,9 @@ function RadarChart({
             />
           ))}
 
-          {axisPaths.map((ap, idx) => (
+          {axisPaths.map((ap, index) => (
             <Path
-              key={'a' + idx}
+              key={index}
               path={ap}
               style="stroke"
               strokeWidth={strokeWidth}
@@ -244,39 +252,39 @@ function RadarChart({
         ]}
         pointerEvents="none"
       >
-        {formatedLabelViews && formatedLabelViews.map((viewDataum, index) => {
-          const top = viewDataum.y;
-          let style: ViewStyle = {
-            position: 'absolute',
-            top: top,
-          };
-          if (viewDataum.align === 'left') style.left = viewDataum.x;
-          if (viewDataum.align === 'right') style.right = viewDataum.x;
+        {formatedLabelViews &&
+          formatedLabelViews.map((viewDataum, index) => {
+            let style: ViewStyle = {
+              position: 'absolute',
+              bottom: viewDataum.y,
+            };
+            if (viewDataum.align === 'left') style.left = viewDataum.x;
+            if (viewDataum.align === 'right') style.right = viewDataum.x;
 
-          return (
-            <View style={style} key={index}>
-              {viewDataum.view}
-            </View>
-          );
-        })}
-        {formatedlabels && formatedlabels.map((formatedlabel, index) => {
-          const top = formatedlabel.y;
-          let style: TextStyle = {
-            position: 'absolute',
-            top: top - 6,
-            fontSize: 12,
-            includeFontPadding: false,
-          };
+            return (
+              <View style={style} key={index}>
+                {viewDataum.view}
+              </View>
+            );
+          })}
+        {formatedlabels &&
+          formatedlabels.map((formatedlabel, index) => {
+            let style: TextStyle = {
+              position: 'absolute',
+              bottom: formatedlabel.y,
+              fontSize: 12,
+              includeFontPadding: false,
+            };
 
-          if (formatedlabel.align === 'left') style.left = formatedlabel.x;
-          if (formatedlabel.align === 'right') style.right = formatedlabel.x;
+            if (formatedlabel.align === 'left') style.left = formatedlabel.x;
+            if (formatedlabel.align === 'right') style.right = formatedlabel.x;
 
-          return (
-            <Text key={index} style={style}>
-              {formatedlabel.label}
-            </Text>
-          );
-        })}
+            return (
+              <Text key={index} style={style}>
+                {formatedlabel.label}
+              </Text>
+            );
+          })}
       </View>
     </View>
   );
